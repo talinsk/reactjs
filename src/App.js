44dea1c82b3d34d74da982d2b11890568ebaa1e1
talinsk <63950363+talinsk@users.noreply.github.com>
2021-02-20 20:29:10 +0700

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { messages: ["old message 1", "old message 2"] };
+  }
+
+  sendMessage() {
+    this.setState({ messages: [...this.state.messages, 'Hi'] });
+  }
+
+  clear() {
+    this.setState({ messages: [] });
+  }
+
+  render() {
+
+    const msgs = this.state.messages.map((m, ind) =>
+      <div key={ind}>{m}</div>
+    );
+
+    return (
+      <div className="App">
+        <main className="app-body">
+          <div>
+            {msgs}
+          </div>
+          <button onClick={() => this.sendMessage()}>Отправить</button>
+          <button onClick={() => this.clear()}>Очистить</button>
+        </main>
+      </div>
+    );
+  }
 }
+
 
 export default App;
