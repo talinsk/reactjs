214ce2d831706afed2b758a59429953b1108a6a2
talinsk <63950363+talinsk@users.noreply.github.com>
2021-02-25 00:34:26 +0700

    lesson2

--- src/AppMessagerFunc.js
+++ src/AppMessagerFunc.js
@@ -1,18 +1,34 @@
 import './AppMessager.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 
 function MessagesFunс() {
   
-  const [messages, setMessages] = useState(["old message 1", "old message 2"]);
+  //sender: 0-user, 1-robot
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // произошло обновление
+    
+    if (messages.length == 0)
+      return;
+
+    const lastEl = messages[messages.length - 1];
+    if (lastEl.sender === 0) {
+      setTimeout(() => {
+        setMessages([...messages, { message: `new message ${messages.length + 1}`, sender: 1, author: "robot" }]);  
+      }, 1000);
+    }
+  });
+
 
   const msgs = messages.map((m, ind) =>
-      <div key={ind}>{m}</div>
+      <div key={ind}>{m.author}: {m.message}</div>
     );
 
   const addMessage = () => {
-    setMessages([...messages, 'Hi']);
+    setMessages([...messages, { message: `new message ${messages.length + 1}`, sender: 0, author: "Me" }]);
   }
 
   const clear = () => {
@@ -22,12 +38,12 @@ function MessagesFunс() {
   return (
     
     <div className="App">
-    <main className="app-body">
+    <main className="app-body">      
+      <button onClick={addMessage}>Отправить</button>
+      <button onClick={clear}>Очистить</button>
       <div>
         {msgs}
       </div>
-      <button onClick={addMessage}>Отправить</button>
-      <button onClick={clear}>Очистить</button>
     </main>
   </div>
   );
